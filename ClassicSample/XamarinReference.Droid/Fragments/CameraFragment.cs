2899d18a50847ea5f99b1c20517b7e5bbc9ebac7
Brad Rosenbaum <bradr@magenic.com>
2016-05-19 08:37:40 -0500

    Camera for Android

--- ClassicSample/XamarinReference.Droid/Fragments/CameraFragment.cs
+++ ClassicSample/XamarinReference.Droid/Fragments/CameraFragment.cs
@@ -11,11 +11,18 @@ using Android.Views;
 using Android.Widget;
 
 using XamarinReference.Droid.Fragments.Base;
+using Java.IO;
+using Android.Provider;
+using Android.Content.PM;
+using Android.Graphics;
 
 namespace XamarinReference.Droid.Fragments
 {
 	public class CameraFragment : BaseFragment
 	{
+		File _dir, _file;
+		Bitmap _bitmap;
+		ImageView _picture;
 		public override void OnCreate (Bundle savedInstanceState)
 		{
 			base.OnCreate (savedInstanceState);
@@ -24,7 +31,110 @@ namespace XamarinReference.Droid.Fragments
 		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 		{
 			var view = inflater.Inflate(Resource.Layout.CameraFragmentLayout, container, false);
+			_picture = view.FindViewById<ImageView>(Resource.Id.imageViewPicture);
+			SetHasOptionsMenu (true);
 			return view;
+		}
+
+		public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
+		{
+			inflater.Inflate(Resource.Menu.CameraMenu, menu);
+			menu.GetItem (0).SetShowAsAction (ShowAsAction.Always);
+		}
+
+		public override bool OnOptionsItemSelected(IMenuItem item)
+		{
+			//camera was clicked
+			if (IsThereAnAppToTakePictures ()) {
+				CreateDirectoryForPictures ();
+				TakeAPicture ();
+			} else {
+				AlertDialog.Builder alert = new AlertDialog.Builder (this.Activity);
+				alert.SetTitle (_localizeLookupService.GetLocalizedString("Confirm"));
+				alert.SetMessage (_localizeLookupService.GetLocalizedString("DeleteFileMessage"));
+				alert.SetNegativeButton ("OK", (senderAlert, args) => {});
+
+				Dialog dialog = alert.Create();
+				dialog.Show();
+			}
+			return true;
+		}
+
+		private void CreateDirectoryForPictures ()
+		{
+			_dir = new File (
+				Android.OS.Environment.GetExternalStoragePublicDirectory (
+					Android.OS.Environment.DirectoryPictures), "CameraAppDemo");
+			if (!_dir.Exists ())
+			{
+				_dir.Mkdirs( );
+			}
+		}
+
+		private bool IsThereAnAppToTakePictures ()
+		{
+			Intent intent = new Intent (MediaStore.ActionImageCapture);
+			IList<ResolveInfo> availableActivities =
+				Activity.PackageManager.QueryIntentActivities (intent, PackageInfoFlags.MatchDefaultOnly);
+			return availableActivities != null && availableActivities.Count > 0;
+		}
+		private void TakeAPicture ()
+		{
+			Intent intent = new Intent (MediaStore.ActionImageCapture);
+			_file = new File (_dir, String.Format("myPhoto_{0}.jpg", Guid.NewGuid()));
+			intent.PutExtra (MediaStore.ExtraOutput, Android.Net.Uri.FromFile (_file));
+			StartActivityForResult (intent, 0);
+		}
+		public override void OnActivityResult (int requestCode, Result resultCode, Intent data)
+		{
+			// Make it available in the gallery
+
+			Intent mediaScanIntent = new Intent (Intent.ActionMediaScannerScanFile);
+			var contentUri = Android.Net.Uri.FromFile (_file);
+			mediaScanIntent.SetData (contentUri);
+			Activity.SendBroadcast (mediaScanIntent);
+
+			// Display in ImageView. We will resize the bitmap to fit the display.
+			// Loading the full sized image will consume to much memory
+			// and cause the application to crash.
+
+			int height = Resources.DisplayMetrics.HeightPixels;
+			int width = _picture.Height ;
+			var bitmap = LoadAndResizeBitmap (_file.Path,width, height);
+			if (bitmap != null) {
+				_picture.SetImageBitmap (bitmap);
+				bitmap = null;
+			}
+
+			// Dispose of the Java side bitmap.
+			GC.Collect();
+		}
+
+		private  Bitmap LoadAndResizeBitmap (string fileName, int width, int height)
+		{
+			// First we get the the dimensions of the file on disk
+			BitmapFactory.Options options = new BitmapFactory.Options { InJustDecodeBounds = true };
+			BitmapFactory.DecodeFile (fileName, options);
+
+			// Next we calculate the ratio that we need to resize the image by
+			// in order to fit the requested dimensions.
+			int outHeight = options.OutHeight;
+			int outWidth = options.OutWidth;
+			int inSampleSize = 1;
+
+			if (outHeight > height || outWidth > width)
+			{
+				inSampleSize = outWidth > outHeight
+					? outHeight / height
+					: outWidth / width;
+			}
+
+			// Now we will load the image and have BitmapFactory resize it for us.
+			options.InSampleSize = inSampleSize;
+			options.InJustDecodeBounds = false;
+			Bitmap resizedBitmap = BitmapFactory.DecodeFile (fileName, options);
+
+			return resizedBitmap;
 		}
 	}
 }
--- ClassicSample/XamarinReference.Droid/Resources/Resource.Designer.cs
+++ ClassicSample/XamarinReference.Droid/Resources/Resource.Designer.cs
@@ -2012,8 +2012,11 @@ namespace XamarinReference.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f090098
-			public const int action0 = 2131296408;
+			// aapt resource value: 0x7f090099
+			public const int action0 = 2131296409;
+			
+			// aapt resource value: 0x7f0900a7
+			public const int actionCamera = 2131296423;
 			
 			// aapt resource value: 0x7f09005a
 			public const int action_bar = 2131296346;
@@ -2039,8 +2042,8 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f09005b
 			public const int action_context_bar = 2131296347;
 			
-			// aapt resource value: 0x7f09009c
-			public const int action_divider = 2131296412;
+			// aapt resource value: 0x7f09009d
+			public const int action_divider = 2131296413;
 			
 			// aapt resource value: 0x7f090004
 			public const int action_menu_divider = 2131296260;
@@ -2072,8 +2075,8 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f090013
 			public const int bottom = 2131296275;
 			
-			// aapt resource value: 0x7f090092
-			public const int buttonDeleteLogFile = 2131296402;
+			// aapt resource value: 0x7f090093
+			public const int buttonDeleteLogFile = 2131296403;
 			
 			// aapt resource value: 0x7f09006f
 			public const int buttonDeveloperOptions = 2131296367;
@@ -2081,14 +2084,14 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f090044
 			public const int buttonPanel = 2131296324;
 			
+			// aapt resource value: 0x7f090092
+			public const int buttonShareLogFile = 2131296402;
+			
 			// aapt resource value: 0x7f090091
-			public const int buttonShareLogFile = 2131296401;
+			public const int buttonViewLogFile = 2131296401;
 			
-			// aapt resource value: 0x7f090090
-			public const int buttonViewLogFile = 2131296400;
-			
-			// aapt resource value: 0x7f090099
-			public const int cancel_action = 2131296409;
+			// aapt resource value: 0x7f09009a
+			public const int cancel_action = 2131296410;
 			
 			// aapt resource value: 0x7f090014
 			public const int center = 2131296276;
@@ -2102,8 +2105,8 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f090052
 			public const int checkbox = 2131296338;
 			
-			// aapt resource value: 0x7f09009f
-			public const int chronometer = 2131296415;
+			// aapt resource value: 0x7f0900a0
+			public const int chronometer = 2131296416;
 			
 			// aapt resource value: 0x7f090079
 			public const int circle1 = 2131296377;
@@ -2138,29 +2141,29 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f090040
 			public const int default_activity_button = 2131296320;
 			
-			// aapt resource value: 0x7f090088
-			public const int design_bottom_sheet = 2131296392;
+			// aapt resource value: 0x7f090089
+			public const int design_bottom_sheet = 2131296393;
+			
+			// aapt resource value: 0x7f090090
+			public const int design_menu_item_action_area = 2131296400;
 			
 			// aapt resource value: 0x7f09008f
-			public const int design_menu_item_action_area = 2131296399;
+			public const int design_menu_item_action_area_stub = 2131296399;
 			
 			// aapt resource value: 0x7f09008e
-			public const int design_menu_item_action_area_stub = 2131296398;
+			public const int design_menu_item_text = 2131296398;
 			
 			// aapt resource value: 0x7f09008d
-			public const int design_menu_item_text = 2131296397;
-			
-			// aapt resource value: 0x7f09008c
-			public const int design_navigation_view = 2131296396;
+			public const int design_navigation_view = 2131296397;
 			
 			// aapt resource value: 0x7f090027
 			public const int disableHome = 2131296295;
 			
-			// aapt resource value: 0x7f090094
-			public const int drawerLayout = 2131296404;
+			// aapt resource value: 0x7f090095
+			public const int drawerLayout = 2131296405;
 			
-			// aapt resource value: 0x7f090096
-			public const int drawerListView = 2131296406;
+			// aapt resource value: 0x7f090097
+			public const int drawerListView = 2131296407;
 			
 			// aapt resource value: 0x7f09005c
 			public const int edit_query = 2131296348;
@@ -2168,8 +2171,8 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f090017
 			public const int end = 2131296279;
 			
-			// aapt resource value: 0x7f0900a4
-			public const int end_padder = 2131296420;
+			// aapt resource value: 0x7f0900a5
+			public const int end_padder = 2131296421;
 			
 			// aapt resource value: 0x7f09000b
 			public const int enterAlways = 2131296267;
@@ -2201,8 +2204,8 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f090070
 			public const int fragment_container = 2131296368;
 			
-			// aapt resource value: 0x7f090095
-			public const int frameLayout = 2131296405;
+			// aapt resource value: 0x7f090096
+			public const int frameLayout = 2131296406;
 			
 			// aapt resource value: 0x7f090006
 			public const int home = 2131296262;
@@ -2219,8 +2222,11 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f09003f
 			public const int image = 2131296319;
 			
-			// aapt resource value: 0x7f0900a3
-			public const int info = 2131296419;
+			// aapt resource value: 0x7f090087
+			public const int imageViewPicture = 2131296391;
+			
+			// aapt resource value: 0x7f0900a4
+			public const int info = 2131296420;
 			
 			// aapt resource value: 0x7f090001
 			public const int item_touch_helper_previous_elevation = 2131296257;
@@ -2228,11 +2234,11 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f090019
 			public const int left = 2131296281;
 			
-			// aapt resource value: 0x7f09009d
-			public const int line1 = 2131296413;
+			// aapt resource value: 0x7f09009e
+			public const int line1 = 2131296414;
 			
-			// aapt resource value: 0x7f0900a1
-			public const int line3 = 2131296417;
+			// aapt resource value: 0x7f0900a2
+			public const int line3 = 2131296418;
 			
 			// aapt resource value: 0x7f090069
 			public const int linearLayout1 = 2131296361;
@@ -2258,17 +2264,17 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f090072
 			public const int listViewBusinessCards = 2131296370;
 			
-			// aapt resource value: 0x7f090097
-			public const int listViewMovies = 2131296407;
+			// aapt resource value: 0x7f090098
+			public const int listViewMovies = 2131296408;
 			
 			// aapt resource value: 0x7f090041
 			public const int list_item = 2131296321;
 			
-			// aapt resource value: 0x7f09009b
-			public const int media_actions = 2131296411;
+			// aapt resource value: 0x7f09009c
+			public const int media_actions = 2131296412;
 			
-			// aapt resource value: 0x7f090093
-			public const int menuRowTextView = 2131296403;
+			// aapt resource value: 0x7f090094
+			public const int menuRowTextView = 2131296404;
 			
 			// aapt resource value: 0x7f090034
 			public const int middle = 2131296308;
@@ -2279,20 +2285,20 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f09002e
 			public const int multiply = 2131296302;
 			
-			// aapt resource value: 0x7f0900a9
-			public const int nav_discussion = 2131296425;
+			// aapt resource value: 0x7f0900ab
+			public const int nav_discussion = 2131296427;
+			
+			// aapt resource value: 0x7f0900aa
+			public const int nav_friends = 2131296426;
 			
 			// aapt resource value: 0x7f0900a8
-			public const int nav_friends = 2131296424;
+			public const int nav_home = 2131296424;
 			
-			// aapt resource value: 0x7f0900a6
-			public const int nav_home = 2131296422;
+			// aapt resource value: 0x7f0900a9
+			public const int nav_messages = 2131296425;
 			
-			// aapt resource value: 0x7f0900a7
-			public const int nav_messages = 2131296423;
-			
-			// aapt resource value: 0x7f09008b
-			public const int navigation_header_container = 2131296395;
+			// aapt resource value: 0x7f09008c
+			public const int navigation_header_container = 2131296396;
 			
 			// aapt resource value: 0x7f090038
 			public const int never = 2131296312;
@@ -2390,11 +2396,11 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f09002b
 			public const int showTitle = 2131296299;
 			
-			// aapt resource value: 0x7f09008a
-			public const int snackbar_action = 2131296394;
+			// aapt resource value: 0x7f09008b
+			public const int snackbar_action = 2131296395;
 			
-			// aapt resource value: 0x7f090089
-			public const int snackbar_text = 2131296393;
+			// aapt resource value: 0x7f09008a
+			public const int snackbar_text = 2131296394;
 			
 			// aapt resource value: 0x7f09000f
 			public const int snap = 2131296271;
@@ -2417,23 +2423,23 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f09001b
 			public const int start = 2131296283;
 			
-			// aapt resource value: 0x7f09009a
-			public const int status_bar_latest_event_content = 2131296410;
+			// aapt resource value: 0x7f09009b
+			public const int status_bar_latest_event_content = 2131296411;
 			
 			// aapt resource value: 0x7f090065
 			public const int submit_area = 2131296357;
 			
-			// aapt resource value: 0x7f0900a5
-			public const int tabFrameLayout = 2131296421;
+			// aapt resource value: 0x7f0900a6
+			public const int tabFrameLayout = 2131296422;
 			
 			// aapt resource value: 0x7f090026
 			public const int tabMode = 2131296294;
 			
-			// aapt resource value: 0x7f0900a2
-			public const int text = 2131296418;
+			// aapt resource value: 0x7f0900a3
+			public const int text = 2131296419;
 			
-			// aapt resource value: 0x7f0900a0
-			public const int text2 = 2131296416;
+			// aapt resource value: 0x7f0900a1
+			public const int text2 = 2131296417;
 			
 			// aapt resource value: 0x7f09004d
 			public const int textSpacerNoButtons = 2131296333;
@@ -2489,8 +2495,8 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f090086
 			public const int textViewToDoValue = 2131296390;
 			
-			// aapt resource value: 0x7f09009e
-			public const int time = 2131296414;
+			// aapt resource value: 0x7f09009f
+			public const int time = 2131296415;
 			
 			// aapt resource value: 0x7f090043
 			public const int title = 2131296323;
@@ -2504,8 +2510,8 @@ namespace XamarinReference.Droid
 			// aapt resource value: 0x7f090047
 			public const int topPanel = 2131296327;
 			
-			// aapt resource value: 0x7f090087
-			public const int touch_outside = 2131296391;
+			// aapt resource value: 0x7f090088
+			public const int touch_outside = 2131296392;
 			
 			// aapt resource value: 0x7f09000a
 			public const int up = 2131296266;
@@ -2787,7 +2793,10 @@ namespace XamarinReference.Droid
 		{
 			
 			// aapt resource value: 0x7f0c0000
-			public const int nav_menu = 2131492864;
+			public const int CameraMenu = 2131492864;
+			
+			// aapt resource value: 0x7f0c0001
+			public const int nav_menu = 2131492865;
 			
 			static Menu()
 			{
